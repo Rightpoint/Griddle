a10cb9325c91e1d685fea09bdd099671c019302b
Andrew Grosner <andrew.grosner@raizlabs.com>
2014-10-07 11:47:25 -0400

    split out the artifactory publisher helper into its own project. now will be referenced accordingly

--- src/main/java/com/raizlabs/android/RaizLibraryPlugin.java
+++ src/main/java/com/raizlabs/android/RaizLibraryPlugin.java
@@ -2,12 +2,6 @@ package com.raizlabs.android;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.jfrog.gradle.plugin.artifactory.ArtifactoryPluginUtil;
-import org.jfrog.gradle.plugin.artifactory.dsl.ArtifactoryPluginConvention;
-import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig;
-import org.jfrog.gradle.plugin.artifactory.extractor.GradleArtifactoryClientConfigUpdater;
-
-import java.lang.reflect.Field;
 
 /**
  * Author: andrewgrosner
@@ -16,31 +10,11 @@ import java.lang.reflect.Field;
  */
 public class RaizLibraryPlugin implements Plugin<Project> {
 
-    public static final String MAVEN = "maven";
-
-    public static final String ARTIFACTORY = "artifactory";
-
     public static final String DEFAULT_GROUP = "rlp_default_group";
 
     public static final String DEFAULT_LIBRARY = "rlp_default_library_directory";
 
     public static final String DEFAULT_LIBRARY_EXTENSION = "rlp_default_library_extension";
-
-    public static final String ARTIFACTORY_CONTEXT_URL = "artifactory_contextUrl";
-
-    public static final String ARTIFACTORY_REPO_ENDPOINT = "/android-dev";
-
-    public static final String ARTIFACTORY_USER = "artifactory_user";
-
-    public static final String ARTIFACTORY_PASSWORD = "artifactory_password";
-
-    public static final String JFROG_VERSION = "2.0.9";
-
-    public static final String JFROG_URL = "http://dl.bintray.com/jfrog/jfrog-jars";
-
-    public static final String GRADLE_TOOLS_VERSION = "0.12.+";
-
-    public static final Object PUBLISHER_REPO_KEY = "android-dev";
 
     public static String GROUP;
 
@@ -50,11 +24,6 @@ public class RaizLibraryPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
-
-        // Retrieve global properties
-        String contextUrl = (String) project.property(ARTIFACTORY_CONTEXT_URL);
-        String user = (String) project.property(ARTIFACTORY_USER);
-        String pass = (String) project.property(ARTIFACTORY_PASSWORD);
 
         // The default group property
         if(project.hasProperty(DEFAULT_GROUP)) {
@@ -78,57 +47,6 @@ public class RaizLibraryPlugin implements Plugin<Project> {
         // Add our methods to the plugins
         project.getConvention().getPlugins().put("RaizCompiler", new RaizDependencyCompiler(project));
         project.getRepositories().mavenCentral();
-
-        // this is for artifactory and releasing builds
-        if(project.hasProperty("publish")) {
-            if(!project.hasProperty("noIncrement")) {
-                project.getConvention().getPlugins().put("RaizBuildIncrementer", new RaizBuildIncrementer(project));
-            }
-
-            // Set up plugins so we never need to add them to a build.gradle
-            project.getPlugins().apply(MAVEN);
-            project.getPlugins().apply(ARTIFACTORY);
-            project.setGroup(GROUP);
-
-            // Add Artifactory repo to the repositories
-            project.getRepositories().maven(new ArtifactoryAction(contextUrl + ARTIFACTORY_REPO_ENDPOINT, user, pass));
-
-            // We will define the plugin convention here so all of our modules do not need to
-            // declare the artifactory closure manually
-            ArtifactoryPluginConvention pluginConvention =
-                    ArtifactoryPluginUtil.getArtifactoryConvention(project);
-            pluginConvention.setContextUrl(contextUrl);
-
-            PublisherConfig publisherConfig = new PublisherConfig(pluginConvention);
-            publisherConfig.setContextUrl(contextUrl);
-            pluginConvention.setPublisherConfig(publisherConfig);
-
-            // Use reflection to access private field
-            PublisherConfig.Repository repository = null;
-
-            Field[] fields = PublisherConfig.class.getDeclaredFields();
-            for(Field field : fields) {
-                if(field.getName().equalsIgnoreCase("repository")) {
-                    try {
-                        field.setAccessible(true);
-                        repository = (PublisherConfig.Repository) field.get(publisherConfig);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            if(repository != null) {
-                repository.setPassword(pass);
-                repository.setUsername(user);
-                repository.setRepoKey(PUBLISHER_REPO_KEY);
-                repository.setMavenCompatible(true);
-            }
-
-            GradleArtifactoryClientConfigUpdater.update(pluginConvention.getClientConfig(), project.getRootProject());
-        } else {
-            System.out.println("Skipping artifactory publishing as publish property is missing. Declare publish=true in your gradle.properties file");
-        }
     }
 
 
